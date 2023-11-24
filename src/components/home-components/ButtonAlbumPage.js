8c9b94b6ec9329868bbfd27cef2a439b4b6fc5f7
João <joaovitorromanoteixeira@gmail.com>
2023-11-24 03:50:07 -0300

    fix2

---
+++ src/components/home-components/ButtonAlbumPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function ButtomAlbumPage() {
+    return (
+    <div>
+        <Link to="/album">
+            <button className="animate ease-in-out duration-300 border p-5
+                mt-10 rounded-xl text-xl bg-neutral-800 text-white animate-bounce"> 
+                Álbum de Fotos
+            </button>
+            </Link>
+    </div>
+    )
+}
