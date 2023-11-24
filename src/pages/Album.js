05d30532c43ad38bea69a374e02bdc67d7fc10ab
João <joaovitorromanoteixeira@gmail.com>
2023-11-24 02:37:48 -0300

    criando states

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,7 +1,12 @@
-
+import React, { useState, useEffect } from 'react';
 
 
 
 const Album = () => {
-    
+    const [photos, setPhotos] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [photosPerPage] = useState(10);
+    const [currentAlbum, setCurrentAlbum] = useState(null)
+    const [albums, setAlbums] = useState([])
+
 }
