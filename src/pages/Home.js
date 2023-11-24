7be59c953df09b400bfbee746448e548d0401832
João <joaovitorromanoteixeira@gmail.com>
2023-11-24 03:23:48 -0300

    pagina home

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import Header from "../components/Header";
+import Cards from "../components/home-components/Cards";
+import ButtomAlbumPage from "../components/home-components/ButtomAlbumPage";
+import Wave from "../components/home-components/Wave";
+import DescriptionHome from "../components/home-components/DescriptionHome";
+import PhoneImg from "../components/home-components/PhoneImg";
+import TitleHome from "../components/home-components/TitleHome";
+
+export default function Home() {
+    return (
+        <div className="w-full h-full bg-cyan-700">
+            <Header />
+            <Wave />
+            <div className="p-2 flex justify-center ">
+                <div className="max-w-7xl flex items-center flex-col">
+                    <TitleHome />
+                    <PhoneImg />
+                    <DescriptionHome />
+                    <Cards />
+                    <ButtomAlbumPage />
+                </div>
+            </div>
+    </div>
+    )
+}
